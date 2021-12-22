c12a9f6f44d893b09ee603b7fd4e9a866e9ba7ed
Vladimir_Polyakov <vladimir_polyakov@epam.com>
2021-12-22 19:26:40 +0400

    Switch to Go module 2

--- module2/main/main.go
+++ module2/main/main.go
@@ -1,5 +1,17 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"github.com/vlpolak/swtgo/hasher"
+)
 
+func main() {
+	password := "password"
+	hash, _ := hasher.HashPassword(password)
+
+	fmt.Println("Password:", password)
+	fmt.Println("Hash:    ", hash)
+
+	match := hasher.CheckPasswordHash(password, hash)
+	fmt.Println("Match:   ", match)
 }
